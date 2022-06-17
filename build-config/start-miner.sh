30ee6aa3ac907b7612998b003d10a724c0608547
source_code <11710298+saad-akhtar@users.noreply.github.com>
2022-06-17 12:02:47 -0500

    Add files via upload

---
+++ build-config/start-miner.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#!/usr/bin/env sh
+#
+#/bin/bash /opt/miner/dbus-wait.sh
+#wait -f
+#if [ "$(df -h /var/data/ | tail -1 | awk '{print $5}' | tr -d '%')" -ge 80 ]; then
+#    rm -rf /var/data/*
+#fi
+#wait -f
+#/bin/bash /opt/miner/bin/miner foreground
+#sleep 5
+#REGIONDATA=$(/opt/miner/bin/miner info region)
+#echo "$REGIONDATA" > /var/pktfwd/region
